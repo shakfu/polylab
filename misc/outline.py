60ed82b1bef3f165bd847d234bb1e56d2f11e604
Shakeeb Alireza <shakeeb.alireza@rezayat.net>
2021-01-09 10:16:00 +0300

    fixes

--- misc/outline.py
+++
@@ -1,8 +0,0 @@
-outline = ['1.12', '1.1', '1.4.1', '1.2']
-outline.sort(key=lambda x: map(int, x.split('.')))
-print outline
-
-
-new_outline = ['.'.join(v) for v in (sorted([k.split('.') for k in
-                                             outline]))]
-
